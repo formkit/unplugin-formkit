a4bf9651959c307b001ee178df8de8390f3e85d8
Justin Schroeder <justin@formkit.com>
2023-09-26 22:28:45 -0400

    chore: externalize deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default <Options>{
   entryPoints: ['src/*.ts'],
   clean: true,
   format: ['cjs', 'esm'],
+  external: ['pathe', '@vue/compiler-dom'],
   dts: true,
   onSuccess: 'npm run build:fix',
 }
